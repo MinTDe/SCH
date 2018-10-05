3f74ba78fc4715c92f9c64f76a9bb493eae6eccd
MinTDe <yc424k@gmail.com>
2018-10-05 17:06:07 +0900

    5 week - C programing homework ver.1.0

---
+++ 2018_c/5_week/homework2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void){
+  int i;
+  for(i=1;i<33;i++){
+    printf("&");
+    if(i % 8 == 0)
+    printf("\n");
+  }
+
+  return 0;
+}
---
+++ 2018_c/5_week/homework3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void){
+  int i = 1;
+  int j = 1;
+  while ( i <= 5 ){
+    j = 1;
+    while ( j <= i){
+      printf(" ");
+      if(i == j){
+        if(i % 2 == 0)
+          printf("O\n");
+        else
+          printf("X\n");
+      }
+      j++;
+    }
+    i++;
+  }
+}
