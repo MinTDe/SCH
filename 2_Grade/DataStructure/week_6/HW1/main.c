0640b3953119694bba0169d0e28194824b737fc0
You_Blue <yc424k@gmail.com>
2021-04-06 17:31:27 +0900

    week6 HW1

---
+++ 2_Grade/DataStructure/week_6/HW1/main.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#define MAX_LIST_SIZE  100
+
+int list[MAX_LIST_SIZE];
+int length = 0;
+
+void init(){
+    length = 0;
+}
+
+int is_full(void){
+    if(length == MAX_LIST_SIZE)
+        return 1;
+    else
+        return 0;
+}
+
+int is_empty(void){
+    if(length==0)
+        return 1;
+    else
+        return 0;
+}
+
+void insert(int pos, int item){
+    int i;
+
+    if(is_full() == 0 && pos >= 0 && pos <= length ){
+        for(i=length; i > pos; i--)
+            list[i] = list[i-1];
+
+        list[pos] = item;
+        length++;
+    }
+    else
+        printf("삽입 에러\n");
+
+}
+void print_list(){
+    printf("(");
+    for(int i = 0; i<length; i++)
+        printf(" %d",list[i]);
+
+    printf(" )\n");
+}
+
+int main() {
+    int num = 10;
+    for(int i =0;i<4;i++)
+        insert(i, num += 10);
+
+    insert(10 ,50);
+    print_list();
+    return 0;
+}
